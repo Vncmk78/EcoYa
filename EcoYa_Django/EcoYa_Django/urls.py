eab3c577752ac7892a0321579c3b343d91e062a7
bliempi2025 <bliempi2025@alu.uct.cl>
2025-11-11 10:53:03 -0300

    Modificacion de urls y views (rutas e index de la pagina)

--- EcoYa_Django/EcoYa_Django/urls.py
+++ EcoYa_Django/EcoYa_Django/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from Producto import views as producto_views
+from . import views as proyecto_views
 #from Producto import views
 
 
@@ -23,4 +26,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('producto', views.inicio, name='inicio_producto'),
     path('producto/', include('Producto.urls')),
+    path('', proyecto_views.index, name='index'),
+    path('obtener/<int:id>', producto_views.Get_producto, name='obtener_producto'),
+    path('actualizar/<int:id>', producto_views.Update_producto, name='actualizar_producto'),
+    path('obtener_todos/', producto_views.GetAll_producto, name='obtener_todos'),
+    path('crear/', producto_views.Create_producto, name='crear_producto'),
+    path('eliminar/<int:id>', producto_views.Delete_producto, name='eliminar_producto'),
 ]
